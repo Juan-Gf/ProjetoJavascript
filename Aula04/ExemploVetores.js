b111e2eab15b150c61564a4ca28b5e1dabd90bbb
Juan G <juangustavo0508@gmail.com>
2025-10-31 14:22:35 -0300

    Aula 4 - Vetores

---
+++ Aula04/ExemploVetores.js
@@ -0,0 +1,8 @@
+let vetorStrings = ["Boxer", "Pastor Alemão", "Pinscher", "Husky Siberiano", "Corgi"]
+
+console.log(vetorStrings[0])
+console.log(vetorStrings[1])
+console.log(vetorStrings[2])
+console.log(vetorStrings[3])
+console.log(vetorStrings[4])
+
