6caa4844e91d5aa010811b2e7245d3d4e9e5cdd8
Juan G <juangustavo0508@gmail.com>
2025-11-12 08:48:45 -0300

    Aula sobre exceções e tratamento de erros

---
+++ Aula13/Exemplo.ts
@@ -0,0 +1,22 @@
+import { converte } from "./Funcoes"
+
+export function main() {
+
+    try {
+        
+        const resultado = converte(123)
+        console.log(resultado)
+    
+    } catch(error) {
+
+        if(error instanceof TypeError) {
+            console.error("TypeError: " + error.message)
+        }
+    } finally {
+        console.log("Sempre vou rodar")
+
+    }
+
+}
+
+main()
